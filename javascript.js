6f60f71d44335fe787cff4a2a43bc4917e3c6f36
kaity6 <202271975+Kaity6@users.noreply.github.com>
2025-07-25 16:33:25 +0000

    to the 4th step

--- javascript.js
+++ javascript.js
@@ -1,15 +1,13 @@
 let container = document.getElementById("container")
+let gridCounter =document.getElementById("gridCoubter")
+
 
 for (let i = 1; i <= 256; i++) {
     let i = document.createElement("div")
     i.classList.add("grid")
     container.appendChild(i)
+    i.addEventListener("mouseover", () =>
+    {
+        i.classList.add("change")
+    })
 }
-let grid = document.querySelectorAll(".grid")
-console.log(grid)
-
-function changeColor() {
-    grid.classList.add("change")
-}
-
-grid.addEventListener("click", changeColor())
